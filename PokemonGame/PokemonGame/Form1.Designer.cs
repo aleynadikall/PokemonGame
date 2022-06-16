a99c20f60ccbfa040e9255e3804fa5a7f6cfe577
Aleyna Dikal <aleyynadikal@gmail.com>
2022-06-16 03:06:18 +0300

    Ash karakteri

---
+++ PokemonGame/PokemonGame/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace PokemonGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Main_panel1 = new System.Windows.Forms.Panel();
+            this.Ash_pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Ash_Movement_timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Main_panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Ash_pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Main_panel1
+            // 
+            this.Main_panel1.BackColor = System.Drawing.Color.Honeydew;
+            this.Main_panel1.Controls.Add(this.Ash_pictureBox1);
+            this.Main_panel1.Location = new System.Drawing.Point(8, 12);
+            this.Main_panel1.Name = "Main_panel1";
+            this.Main_panel1.Size = new System.Drawing.Size(789, 777);
+            this.Main_panel1.TabIndex = 0;
+            // 
+            // Ash_pictureBox1
+            // 
+            this.Ash_pictureBox1.BackgroundImage = global::PokemonGame.Properties.Resources.Ash_Front_1;
+            this.Ash_pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Ash_pictureBox1.Location = new System.Drawing.Point(240, 240);
+            this.Ash_pictureBox1.Name = "Ash_pictureBox1";
+            this.Ash_pictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.Ash_pictureBox1.TabIndex = 0;
+            this.Ash_pictureBox1.TabStop = false;
+            // 
+            // Ash_Movement_timer1
+            // 
+            this.Ash_Movement_timer1.Interval = 200;
+            this.Ash_Movement_timer1.Tick += new System.EventHandler(this.Ash_Movement_timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 792);
+            this.Controls.Add(this.Main_panel1);
+            this.Name = "Form1";
+            this.Text = "Pokemon";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.Main_panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Ash_pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Main_panel1;
+        private System.Windows.Forms.PictureBox Ash_pictureBox1;
+        private System.Windows.Forms.Timer Ash_Movement_timer1;
+    }
+}
+
---
+++ PokemonGame/PokemonGame/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokemonGame
+{
+    public partial class Form1 : Form
+    {
+        bool Ash_Front = true;
+        bool Ash_Back = false;
+        bool Ash_Left = false;
+        bool Ash_Right = false;
+        bool Ash_Stop = true;
+        bool Ash_First_Movement = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down || e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+            {
+                Ash_Front = false;
+                Ash_Back = false;
+                Ash_Left = false;
+                Ash_Right = false;
+                Ash_Movement_timer1.Enabled = true;
+                Ash_Stop = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                Ash_Back = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                Ash_Front = true;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                Ash_Left = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                Ash_Right = true;
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down || e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+            {
+                Ash_Stop = true;
+            }
+        }
+
+        private void Ash_Movement_timer1_Tick(object sender, EventArgs e)
+        {
+            Image img = null;
+
+            if(!Ash_Stop)
+            {
+                if(Ash_First_Movement)
+                {
+                    if (Ash_Front)
+                    {
+                        img = Properties.Resources.Ash_Front_2;
+                    }
+                    else if (Ash_Back)
+                    {
+                        img = Properties.Resources.Ash_Back_2;
+
+                    }
+                    else if (Ash_Left)
+                    {
+                        img = Properties.Resources.Ash_Left_2;
+
+                    }
+                    else if (Ash_Right)
+                    {
+                        img = Properties.Resources.Ash_Right_2;
+
+                    }
+                    Ash_First_Movement = false;
+                }
+                else
+                {
+                    if (Ash_Front)
+                    {
+                        img = Properties.Resources.Ash_Front_3;
+                    }
+                    else if (Ash_Back)
+                    {
+                        img = Properties.Resources.Ash_Back_3;
+
+                    }
+                    else if (Ash_Left)
+                    {
+                        img = Properties.Resources.Ash_Left_1;
+
+                    }
+                    else if (Ash_Right)
+                    {
+                        img = Properties.Resources.Ash_Right_1;
+
+                    }
+                    Ash_First_Movement = true;
+
+                }
+
+                if(Ash_Front)
+                {
+                    Ash_pictureBox1.Top = Ash_pictureBox1.Top + 80;
+                }
+                else if(Ash_Back)
+                {
+                    Ash_pictureBox1.Top = Ash_pictureBox1.Top - 80;
+
+                }
+                else if (Ash_Left)
+                {
+                    Ash_pictureBox1.Left = Ash_pictureBox1.Left - 80;
+
+                }
+                else if (Ash_Right)
+                {
+                    Ash_pictureBox1.Left = Ash_pictureBox1.Left + 80;
+
+                }
+            }
+            else
+            {
+                Ash_First_Movement = true;
+                Ash_Movement_timer1.Enabled = false;
+                if (Ash_Front)
+                {
+                    img = Properties.Resources.Ash_Front_1;
+                }
+                else if (Ash_Back)
+                {
+                    img = Properties.Resources.Ash_Back_1;
+
+                }
+                else if (Ash_Left)
+                {
+                    img = Properties.Resources.Ash_Left_1;
+
+                }
+                else if (Ash_Right)
+                {
+                    img = Properties.Resources.Ash_Right_1;
+
+                }
+            }
+            Ash_pictureBox1.BackgroundImage = img;
+        }
+    }
+}
